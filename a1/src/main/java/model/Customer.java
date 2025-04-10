62fd354711c14ca71153bcb9eb9764dc6608d3a2
Plane <flying.pig121212@gmail.com>
2025-04-10 20:46:11 +1000

    test(customer model)

---
+++ a1/src/main/java/model/Customer.java
@@ -0,0 +1,62 @@
+package model;
+
+
+public class Customer {
+
+    private String FirstName;
+    private String LastName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+
+    public Customer(String firstName, String lastName, String phoneNumber, String email, String password) {
+        FirstName = firstName;
+        LastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
