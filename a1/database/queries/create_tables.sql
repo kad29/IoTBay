615ae2586b5fb035b477080a3a4afcf90232f6d0
kad29 <kade.lopez@student.uts.edu.au>
2025-05-27 00:21:40 +1000

    added dao file

--- a1/database/queries/create_tables.sql
+++ a1/database/queries/create_tables.sql
@@ -1,10 +1,4 @@
-CREATE TABLE Customer (
-    name VARCHAR(20),
-    username VARCHAR(20),
-    email VARCHAR(30),
-    phone integer(15),
-    password VARCHAR(30)
-);
+
 
 CREATE TABLE Product (
     productID int(10),
