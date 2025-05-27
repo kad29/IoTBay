d7a6ae6545e6c1c2e9d526da48814b8b6779b0f5
BachMoi <phamquangbach0412@gmail.com>
2025-05-27 21:39:19 +1000

    db

--- a1/src/main/java/model/dao/DB.java
+++ a1/src/main/java/model/dao/DB.java
@@ -1,1 +1,14 @@
+import java.sql.Connection;
 
+/** 
+* Super class of DAO classes that stores the database information 
+*  
+*/
+
+public abstract class DB {   
+
+protected String URL = "jdbc:derby://localhost:8080/";//replace this string with your jdbc:derby local host url   
+protected String driver = "org.apache.derby.jdbc.ClientDriver"; //jdbc client driver - built in with NetBeans   
+protected Connection conn; //connection null-instance to be initialized in sub-classes
+
+}
